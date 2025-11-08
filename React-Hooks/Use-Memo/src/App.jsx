065f4f64c994b321b1d372186a1efb1d82752da3
AdityaAnand0108 <129715108+AdityaAnand0108@users.noreply.github.com>
2025-11-08 13:07:08 +0530

    Passing Inline Functions — Why It Breaks memoization
    Problem:
    You have a memoized child, but every time you pass an inline arrow function (like () => setCount(count+1)), the child still re-renders.
    Fix it.

--- React-Hooks/Use-Memo/src/App.jsx
+++ React-Hooks/Use-Memo/src/App.jsx
@@ -1,17 +1,24 @@
-import {useState} from 'react'
+import {useCallback, useState} from 'react'
 import './App.css'
 import Navbar from "./component/Navbar.jsx";
 import CardComponent from "./component/CardComponent.jsx";
 
 function App() {
     const [count, setCount] = useState(0)
+    const [item , setItem] = useState(0)
+
+    // const handleIncrementChild = () => { setItem(item + 1) } //Without useCallBack it will re- intiate this function so memo will think it as new function and props value change so child component re-render
+
+    const handleIncrementChild = useCallback(() => {
+        setItem(item + 1)
+    }, [item])
 
     return (
         <>
-            < CardComponent title={"LTIMindtree"} description={"Fullstack Stack Developer"}/>
+            < CardComponent title={"LTIMindtree"} description={"Fullstack Stack Developer"} handleIncrement = {handleIncrementChild} item = { item } />
             <h1>Aditya Anand Mishra</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
+                <button onClick={() => setCount(count + 1)}>
                     count is {count}
                 </button>
             </div>
--- React-Hooks/Use-Memo/src/component/CardComponent.jsx
+++ React-Hooks/Use-Memo/src/component/CardComponent.jsx
@@ -1,6 +1,6 @@
 import React, {memo} from "react";
 
-const CardComponent = ({ title, description}) => {
+const CardComponent = ({ title, description, handleIncrement, item}) => {
     console.log("Child component Rendered")
     return (
         <div className="max-w-sm bg-white border border-gray-200 rounded-2xl shadow-md overflow-hidden m-4 transition-transform duration-200 hover:scale-105 hover:shadow-lg">
@@ -10,6 +10,12 @@ const CardComponent = ({ title, description}) => {
                 <p className="text-gray-600 text-sm mb-4">
                     {description}
                 </p>
+                <button
+                    onClick={handleIncrement}
+                    className="w-full bg-blue-600 text- text-sm font-medium py-2 rounded-lg hover:bg-blue-700 transition-colors"
+                >
+                    Item added to cart : {item}
+                </button>
             </div>
         </div>
     );
